47e3d5abd775be935925c1e35afd7d1b7928c53b
Ayush Kumar Gupta <ayush.gupta@sprinklr.com>
2022-05-29 23:59:23 +0530

    improve performance chart styling

--- js/performance-metrics.js
+++ js/performance-metrics.js
@@ -6,15 +6,13 @@ function renderPerformanceMetrics() {
   const appCache = document.querySelector('#app-cache');
   const dns = document.querySelector('#dns');
   const tcp = document.querySelector('#tcp');
-  const request = document.querySelector('#request');
-  const response = document.querySelector('#response');
+  const requestResponse = document.querySelector('#request-response');
   const processing = document.querySelector('#processing');
   const onLoad = document.querySelector('#on-load');
   let performance = JSON.parse(localStorage.getItem('performance'));
   if (performance.length > 0) {
     performance = performance[0];
-    request.innerHTML = round(performance.requestStart) + "ms";
-    response.innerHTML = round(performance.responseStart) + "ms";
+    requestResponse.innerHTML = round(performance.requestStart) + "ms";
     processing.innerHTML = round(performance.responseEnd) + "ms";
     onLoad.innerHTML = round(performance.loadEventStart) + "ms";
     tcp.innerHTML = round(performance.connectStart) + "ms";
